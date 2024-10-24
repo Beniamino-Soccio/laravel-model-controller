1744b8e3cbf27cc0abdddf49efd5998c72e8b4a1
Beniamino-Soccio <beniamino43@gmail.com>
2024-10-24 18:07:59 +0200

    added movies cards + end of exercise

--- resources/views/movies/index.blade.php
+++ resources/views/movies/index.blade.php
@@ -5,10 +5,21 @@
 @section("main-content")
 
     <div class="container">
-        <div class="row">
-            <div class="col-3">
-                ciao
-            </div>
+        <h1 class="fw-bold text-center p-5">Movies</h1>
+        <div class="row justify-content-center">
+            @forelse ( $movies as $movie )
+                <div class="card col-5 m-2">
+                    <div class="card-body">
+                        <h4 class="card-title"> {{ $movie->title }} </h4>
+                        <p class="card-text"> <span class="fw-bold">Original Name:</span> {{ $movie->original_title }} </p>
+                        <p class="card-text"> <span class="fw-bold">Nationality:</span> {{ $movie->nationality }} </p>
+                        <p class="card-text"> <span class="fw-bold">Date:</span> {{ $movie->date }} </p>
+                        <p class="card-text"> <span class="fw-bold">Vote:</span> {{ $movie->vote }} </p>
+                    </div>
+                </div>
+            @empty
+                <p>movies are not available</p>
+            @endforelse
         </div>
     </div>
 
