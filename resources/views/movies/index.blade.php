fc4588fd25375300bcc78547d02b3fa205c2b97c
Beniamino-Soccio <beniamino43@gmail.com>
2024-10-24 17:52:12 +0200

    added route + created controller and model

---
+++ resources/views/movies/index.blade.php
@@ -0,0 +1,15 @@
+@extends("layouts.app")
+
+@section("page-title", "Movies")
+
+@section("main-content")
+
+    <div class="container">
+        <div class="row">
+            <div class="col-3">
+                ciao
+            </div>
+        </div>
+    </div>
+
+@endsection
